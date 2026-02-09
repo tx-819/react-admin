eec2081a1e6812257df8474f7745cd7cfd685c65
Tang Yinxuan <tyx923233700@gmail.com>
2026-02-09 09:29:32 +0800

    fix: NProgressFallback

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "antd/dist/reset.css";
+import "nprogress/nprogress.css";
 import "./index.css";
 import "@/utils/i18n";
 import App from "./App";
